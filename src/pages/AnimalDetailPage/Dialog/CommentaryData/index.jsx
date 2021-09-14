20ca775bafe42cd6133575a75f46f9ffed5a73d6
emersonpuma777 <emerson.puma@ideascloud.io>
2021-09-14 12:05:59 -0500

    refactory view animal

--- src/pages/AnimalDetailPage/Dialog/CommentaryData/index.jsx
+++ src/pages/AnimalDetailPage/Dialog/CommentaryData/index.jsx
@@ -1,24 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { Grid, Typography } from '@material-ui/core'
-import { CustomForm } from '../../../../Components/CustomForm'
-import { commentaryForm } from './constants'
-import { useStyles } from './styles'
+import React, { useEffect, useState } from "react";
+import { Grid, Typography } from "@material-ui/core";
+import { CustomForm } from "../../../../Components/CustomForm";
+import { commentaryForm } from "./constants";
+import { useStyles } from "./styles";
+
+/**
+ * @component
+ * @description Componente, renderiza un dialog
+ * @author Emerson Puma Quispe <emerson.puma@ideascloud.io>
+ */
 
 function CommentaryData({ setOpen, setAnimalsList, agribusinessId }) {
-  const classes = useStyles()
+  const classes = useStyles();
   const [sections] = useState([
     {
-      title: '',
+      title: "",
       form: commentaryForm(),
     },
-  ])
+  ]);
 
   return (
     <Grid className={classes.modal}>
-      <Typography variant={'subtitle1'}>Comentario</Typography>
+      <Typography variant={"subtitle1"}>Comentario</Typography>
       <CustomForm sections={sections} handlePrev={true}></CustomForm>
     </Grid>
-  )
+  );
 }
 
-export default CommentaryData
+export default CommentaryData;
